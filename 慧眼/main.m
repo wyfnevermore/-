6d3396d18883ff15c89ba5d57938a549d6fe7f9f
wyfnevermore <843645610@qq.com>
2017-03-16 19:53:43 +0800

    Initial Commit

---
+++ 慧眼/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  慧眼
+//
+//  Created by wyfnevermore on 2017/3/16.
+//  Copyright © 2017年 wyfnevermore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
